feaa0f4d674ac8861cbadf1410d07c8fb7c9ebff
zhaonan.chen <zhaonan.chen@zhaonanchendeMacBook-Air.local>
2026-05-01 22:13:19 +0800

    feat: 导航栏

---
+++ src/views/tabs/Tabbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <van-tabbar v-model="active">
+    <van-tabbar-item name="home" icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item name="order" icon="bars">订单</van-tabbar-item>
+    <van-tabbar-item name="my" icon="contact">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const active = ref('home')
+</script>
+
+<style scoped></style>
